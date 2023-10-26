cd3cd1ba5451836238e322899c26691873c01c60
NicoAcostaX0 <fabrinico25@gmail.com>
2023-10-25 21:17:48 -0300

    Nico

--- JS/main.js
+++ JS/main.js
@@ -1,49 +1,87 @@
-function validarFormulario() {
-  var nombre = document.getElementById("nombre").value;
-  var email = document.getElementById("email").value;
-  var mensaje = document.getElementById("mensaje").value;
+let head =  `<header>
+<nav>
+    <input type="checkbox" id="check">
+    <label for="check" class="checkbtn">
+        <img class="icon_bar" src="./img/icons/bar-de-hamburguesas.png" alt="">
+    </label>
 
-  if (nombre === "" || email === "" || mensaje === "") {
-    alert("Por favor, complete todos los campos.");
-    return false;
-  }
-  alert("Formulario validado con éxito.");
-  return true;
-}
+    <a href="./index.html" class="enlace"><img class="logo" src="./img/icons/artificial.png" alt="Logo"></a>
+    <div class="nav-main">
+        <ul class="open">
+            <li><a class="active" href="./index.html">Introducción</a></li>
+            <li><a  href=" ./tecnologias.html">Inteligencias Artificiales</a></li>
+            <li><a href="./nosotros.html">Nosotros</a></li>
+            <li><a href="./faq.html">FAQ</a></li>
 
-let general = `<nav>
-<a href="#"><img class="logo" src="./img/artificial.png" alt="Logo"></a>
-<div class="nav-links">
-    <div class="nav-bg">
-        <a href="./index.html" class="nav-link">Introducción</a>
-        <a " href="./tecnologiasia.html" class="nav-link">Inteligencias Artificiales</a>
-        <a href="./nosotros.html" class="nav-link">Nosotros</a>
-        <a href="./faq.html" class="nav-link">FAQ</a>
+            <li><a  href=" ./iniciosesion.html">Iniciar Sesión</a></li>
+            <li><a target="_blank" href="./registrate.html">Regístrate</a></li>
+            <li><a href="./contacto.html">Contacto</a></li>
+        </ul>
     </div>
-    <div class="nav-actions">
-        <a " href="./Acceso.html" class="nav-action">Iniciar Sesión</a>
-        <a target="_blank" href="./registrate.html" class="nav-action">Regístrate</a>
-    </div>
-    <div class="nav-contacts">
-    <a href="./contacto.html" class="nav-contact">Contacto</a>
-    </div>
-
-</div>
-</nav>`;
-
-document.querySelector("header").innerHTML = general;
+</nav>
+</header>`
+document.querySelector("header").innerHTML = head;
 
 let foot = `<footer>
     <div class="copyright">
         2023 <b> IA Technologies</b>&copy; Todos los derechos Reservados
     </div>
     <div class="social-icons">
-        <a href="index.html"><img src="./img/email.png" alt=""></a>
-        <a href="index.html"><img src="./img/facebook.png" alt="Facebook"></a>
-        <a href="index.html"><img src="./img/instagram.png" alt="Instagram"></a>
-        <a href="index.html"><img src="./img/twitter.png" alt="Twitter"></a>
-        <a href="index.html"><img src="./img/github.png" alt="GitHub"></a>
-        <a href="index.html"><img src="./img/youtube.png" alt="YouTube"></a>
+        <a href="index.html"><img src="./img/icons/email.png" alt=""></a>
+        <a href="index.html"><img src="./img/icons/facebook.png" alt="Facebook"></a>
+        <a href="index.html"><img src="./img/icons/instagram.png" alt="Instagram"></a>
+        <a href="index.html"><img src="./img/icons/twitter.png" alt="Twitter"></a>
+        <a href="index.html"><img src="./img/icons/github.png" alt="GitHub"></a>
+        <a href="index.html"><img src="./img/icons/youtube.png" alt="YouTube"></a>
     </div>
 </footer>`;
 document.querySelector("footer").innerHTML = foot;
+
+document.getElementById('check').addEventListener('change', function() {
+    const navUl = document.querySelector('.nav-main ul');
+    navUl.classList.toggle('active', this.checked);
+});
+
+
+function validarFormulario() {
+    var nombre = document.getElementById("nombre").value;
+    var email = document.getElementById("email").value;
+    var mensaje = document.getElementById("mensaje").value;
+  
+    if (nombre === "" || email === "" || mensaje === "") {
+      alert("Por favor, complete todos los campos.");
+      return false;
+    }
+    alert("Formulario validado con éxito.");
+    return true;
+  }
+
+
+  function validarUsuario() {
+    var usuario = document.getElementById("usuario").value;
+    var clave = document.getElementById("clave").value;
+
+    // Validar el campo de usuario
+    if (usuario === "") {
+        document.getElementById("mensajeUsuario").innerHTML = "Complete este campo";
+        return false;
+    } else {
+        document.getElementById("mensajeUsuario").innerHTML = "";
+    }
+
+    // Validar el campo de clave
+    if (clave === "") {
+        document.getElementById("mensajeClave").innerHTML = "Complete este campo";
+        return false;
+    } else {
+        document.getElementById("mensajeClave").innerHTML = "";
+    }
+    
+    if (usuario === "usuario" && clave === "contrasena") {
+        alert("Inicio de sesión exitoso");
+    } else {
+        alert("Usuario o clave incorrectos");
+    }
+
+    return true;
+}
--- JS/tech.js
+++ JS/tech.js
@@ -1,25 +1,43 @@
-const { createApp } = Vue
+const { createApp } = Vue;
+
 createApp({
   data() {
     return {
       url: "./JS/datos.json",
       error: false,
       datos: [],
-    }
+      seccionSeleccionada: "", 
+    };
+  },
+  computed: {
+    elementosFiltrados() {
+      // Filtrar elementos según la sección seleccionada
+      if (!this.seccionSeleccionada) {
+        return this.datos;
+      } else {
+        return this.datos.filter(elemento => elemento.seccion === this.seccionSeleccionada);
+      }
+    },
+    secciones() {
+      // Obtener una lista de secciones 
+      return [...new Set(this.datos.map(elemento => elemento.seccion))];
+    },
   },
   methods: {
     fetchdata(url) {
       fetch(url)
-        .then(response => response.json())
-        .then(data => {
-          console.log(data);
-          this.datos = data.resultados
-        }
-
-        );
+        .then((response) => response.json())
+        .then((data) => {
+          this.datos = data.resultados;
+          console.log(this.datos)
+        })
+        .catch((error) => {
+          this.error = true;
+          console.error(error);
+        });
     },
   },
-  created(){
-    this.fetchdata(this.url)
+  created() {
+    this.fetchdata(this.url);
   },
-}).mount('#app')
+}).mount("#app");
